7387d11ed969aefbaa88094737d983acae5301ae
Xiana <xcarrera.alonso@gmail.com>
2022-12-07 22:53:27 +0100

    ESACO

--- acov2.hpp
+++ acov2.hpp
@@ -62,6 +62,42 @@ vvi determine_initial_candidate_lists(int n, vvi dist_matrix, int candidate_list
 }
 
 
+
+queue<int> determine_ESACO_candidate_list(int n, vvi dist_matrix, uset unvisited, int candidate_list_size, int current_city){
+    uset unvisited_copy = unvisited;
+    queue<int> candidate_list;
+    for (int i = 0; i < candidate_list_size; i++){
+        int min_dist = INF;
+        int min_dist_city = -1;
+        uset::iterator it;
+        for (it = unvisited_copy.begin(); it != unvisited_copy.end(); ++it){
+            if (dist_matrix[current_city][*it] < min_dist){
+                min_dist = dist_matrix[current_city][*it];
+                min_dist_city = *it;
+            }
+        }
+        candidate_list.push(min_dist_city);
+        unvisited_copy.erase(min_dist_city);
+    }
+    if (unvisited.size() != n - 1) cout << "ERROR: unvisited size is not n - 1" << endl;
+    return candidate_list;
+}
+
+vector<queue<int>> determine_ESACO_initial_candidate_lists(int n, vvi dist_matrix, int candidate_list_size){
+    vector<queue<int>> candidate_lists(n);
+    uset unvisited;
+    for (int i = 0; i < n; i++){
+        unvisited.insert(i);
+    }
+    for (int i = 0; i < n; i++){
+        unvisited.erase(i);
+        candidate_lists[i] = determine_ESACO_candidate_list(n, dist_matrix, unvisited, candidate_list_size, i);
+        unvisited.insert(i);
+    }
+    return candidate_lists;
+}
+
+
 int choose_with_list(int n, vvi dist_matrix, vector<vector<float>> pheromone_matrix, int current_city, vi candidate_list, vector<bool> visited, int cl_size, int Q0, float beta){
     // Choose next city
     // 95% of the time, choose the best edge according to pheromone (exploitation)
@@ -72,6 +108,7 @@ int choose_with_list(int n, vvi dist_matrix, vector<vector<float>> pheromone_mat
         float best_transition = -1.0f;
         for (int i = 0; i < cl_size; i++){
             int city = candidate_list[i];
+            if (current_city == city) continue;
             if (visited[city]) continue;
             float edge_transition = pheromone_matrix[current_city][city] * pow(1.0 / dist_matrix[current_city][city], beta);
             if (edge_transition > best_transition){
@@ -80,16 +117,18 @@ int choose_with_list(int n, vvi dist_matrix, vector<vector<float>> pheromone_mat
             }
         }
     } else {                     // Exploration
-        vector<float> probabilities(n, 0);    // Probabiliy of visiting city l from the current city
+        vector<float> probabilities(n, 0.0f);    // Probabiliy of visiting city l from the current city
         float denominator = 0.0f;
         for (int i = 0; i < cl_size; i++){
             int city = candidate_list[i];
+            if (current_city == city) continue;
             if (visited[city]) continue;
             denominator += pheromone_matrix[current_city][city] * pow(1.0 / dist_matrix[current_city][city], beta);
         }
 
         for (int i = 0; i < cl_size; i++){
             int city = candidate_list[i];
+            if (current_city == city) continue;
             if (visited[city]) continue;
             probabilities[city] = pheromone_matrix[current_city][city] * pow(1.0 / dist_matrix[current_city][city], beta) / denominator;
         }
@@ -113,6 +152,7 @@ int choose_with_uset(int n, vvi dist_matrix, vector<vector<float>> pheromone_mat
         // Choose best edge
         float best_transition = -1.0f;
         for (int i = 0; i < n; i++){
+            if (current_city == i) continue;
             if (visited[i]) continue;
             float edge_transition = pheromone_matrix[current_city][i] * pow(1.0 / dist_matrix[current_city][i], beta);
             if (edge_transition > best_transition){
@@ -124,11 +164,13 @@ int choose_with_uset(int n, vvi dist_matrix, vector<vector<float>> pheromone_mat
         vector<float> probabilities(n, 0);    // Probabiliy of visiting city l from the current city
         float denominator = 0.0f;
         for (int i = 0; i < n; i++){
+            if (current_city == i) continue;
             if (visited[i]) continue;
             denominator += pheromone_matrix[current_city][i] * pow(1.0 / dist_matrix[current_city][i], beta);
         }
         
         for (int i = 0; i < n; i++){
+            if (current_city == i) continue;
             if (visited[i]) continue;
             probabilities[i] = pheromone_matrix[current_city][i] * pow(1.0 / dist_matrix[current_city][i], beta) / denominator;
         }
@@ -147,7 +189,7 @@ vi ACO_candidate(Problem * problem)
 
     int n = problem->n;
     int numIterations = 0;         // End condition for the main loop
-    int candidate_list_size = 15;  // Size of the candidate list
+    int candidate_list_size = n / 4;  // Size of the candidate list
     float alpha = 2.0f;
     float phi = 0.1f;             
     float beta = 2.0f;              // Importance of distance over pheromone  
@@ -159,7 +201,7 @@ vi ACO_candidate(Problem * problem)
     //float tau0 = 1.0f / (n * length_best_NN(problem)); // Initial pheromone value
     //float tau0 = 1.0f / (n * length_NN(problem, rand() % n)); // Initial pheromone value
     //float tau0 = 1000 * n / ((float) length_NN(problem, rand() % n)); // Initial pheromone value
-    float tau0 = 1 / (n * (float) length_NN(problem, rand() % n)); // Initial pheromone value
+    float tau0 = 1.0f / (n * (float) length_NN(problem, rand() % n)); // Initial pheromone value
     //float tau0 = 100.0f;
     float tau;
 
@@ -280,5 +322,200 @@ vi ACO_candidate(Problem * problem)
     }
     return best_ant_sol;
 }
+
+vvi ESACO_update_candidate_lists(vvi old_candidate_lists, vector<vector<float>> pheromone_matrix, int n, vi best_ant_sol){
+    // Update candidate lists
+    vvi new_candidate_lists(n);
+    for (int i = 0; i < n; i++){
+        vi candidate_list = old_candidate_lists[i];
+
+        int best_pheromone = -1;
+        int best_neighbour = -1;
+        for (int j = 0; j < n; j++){
+            if (i == j) continue;
+            if (pheromone_matrix[i][j] > best_pheromone){
+                best_pheromone = pheromone_matrix[i][j];
+                best_neighbour = j;
+            }
+        }
+
+        // Remove last node of candidates_list
+        candidate_list.pop_back();
+        // Insert best neighbour at the beginning of the list
+        candidate_list.insert(candidate_list.begin(), best_neighbour);
+        // Remove last node of candidates_list
+        candidate_list.pop_back();
+        // Insert successor of i in best tour as first in the candidates_list
+        auto idx = find(best_ant_sol.begin(), best_ant_sol.end(), i) + 1 - best_ant_sol.begin();
+        if (idx == n) idx = 0;
+        int successor = best_ant_sol[idx];
+        candidate_list.insert(candidate_list.begin(), successor);
+
+        new_candidate_lists[i] = candidate_list;
+    }
+    return new_candidate_lists;
+}
+
+
+/*
+ * 1. Candidate lists as queues
+ */
+vi ESACO(Problem * problem)
+{
+    float start_time = (float) clock();
+
+    int n = problem->n;
+    int numIterations = 0;         // End condition for the main loop
+    int candidate_list_size = n / 4;  // Size of the candidate list
+    float phi = 0.15f;             
+    float beta = 2.0f;              // Importance of distance over pheromone  
+    float rho = 0.15f;               // Evaporation rate
+    int Q0 = 95;                    // Probability of choosing the best solution
+    int m = 10;                     // Number of ants
+
+    // TODO: Initialize pheromone matrix
+    //float tau0 = 1.0f / (n * length_best_NN(problem)); // Initial pheromone value
+    float tau0 = n / ((float) length_NN(problem, rand() % n)); // Initial pheromone value
+    //float tau0 = 1000 * n / ((float) length_NN(problem, rand() % n)); // Initial pheromone value
+    //float tau0 = 1 / (n * (float) length_NN(problem, rand() % n)); // Initial pheromone value
+    //float tau0 = 100.0f;
+    float tau;
+
+    // Pheromone matrix
+    vector<vector<float>> pheromone_matrix(n, vector<float>(n, tau0));
+    vvi candidate_lists = determine_initial_candidate_lists(n, problem->dist_matrix, candidate_list_size);
+
+
+    // m = 10 from original paper
+    // Ants and their paths
+    vector<vi> ants(m, vi(n, 0));
+
+    int best_ant_length = INF;      // Global histórico
+    vi best_ant_sol;                // Global histórico    
+
+    int next_city;               
+
+    while ((((float) clock()) - start_time) / CLOCKS_PER_SEC < 60){         // Run for 3 minutes
+        vector<vector<bool>> visited = vector<vector<bool>>(m, vector<bool>(n, false)); // Unordered set of visited cities, one set for each ant
+
+        // Put each ant in a random initial city
+        for (int k = 0; k < m; k++){        // For each ant
+            int start_city = rand() % n;
+            ants[k][0] = start_city;
+            visited[k][start_city] = true;     // Remove initial city from unvisited
+        }
+
+        // Unordered set of visited cities, one set for each ant
+        
+
+        for (int j = 0; j < n - 1; j++){     // For each city
+            for (int k = 0; k < m; k++){        // For each ant
+                vector<bool> ant_visited = visited[k];
+                int current_city = ants[k][j];
+                vi candidate_list = candidate_lists[current_city];
+
+                // Choose next city
+                // 95% of the time, choose the best edge according to pheromone (exploitation)
+                // 5% of the time, choose a random edge based on probability (exploration)
+                next_city = -1;
+                float thereAreCandidates = false;
+                for (int l = 0; l < candidate_list_size; l++){
+                    if (!ant_visited[candidate_list[l]]){
+                        thereAreCandidates = true;
+                        break;
+                    }
+                }
+
+                if (thereAreCandidates){
+                    next_city = choose_with_list(n, problem->dist_matrix, pheromone_matrix, current_city, candidate_list, ant_visited, candidate_list_size, Q0, beta);          
+                } else {
+                    next_city = choose_with_uset(n, problem->dist_matrix, pheromone_matrix, current_city, visited[k], Q0, beta);    
+                }
+
+
+
+                
+                // Update ant's path
+                ants[k][j + 1] = next_city;
+                // Remove next city from unvisited
+                visited[k][next_city] = true;
+                
+                for(int i = 0; i < j + 1; i++) {
+                    if (ants[k][i] == ants[k][j + 1]) {
+                        cout << "ERROR: Ant " << k << " has visited city " << ants[k][i] << " twice" << endl;
+                        cout << "j" << j << endl;
+                        cout << "Ant's path: ";
+                        for (int i = 0; i < n; i++) {
+                            cout << ants[k][i] << " ";
+                        }
+                        cout << thereAreCandidates << endl;
+                        exit(1);
+                    }
+                }
+                
+                // Update pheromone matrix
+                tau = (1 - rho) * pheromone_matrix[current_city][next_city] + rho * tau0;
+                pheromone_matrix[current_city][next_city] = tau;
+                pheromone_matrix[next_city][current_city] = tau;
+            }
+        }
+        
+        for (int k = 0; k < m; k++){
+            // Update pheromone from last city to first city
+            tau = (1 - rho) * pheromone_matrix[next_city][ants[k][0]] + rho * tau0;
+            pheromone_matrix[next_city][ants[k][0]] = tau;
+            pheromone_matrix[ants[k][0]][next_city] = tau;
+            
+            ants[k] = two_opt_greedy(problem, ants[k]);
+
+            int ant_length = compute_length(problem, ants[k]);
+            if (ant_length < best_ant_length){
+                best_ant_length = ant_length;
+                best_ant_sol = ants[k];
+                /*
+                for (int i = 0; i < n; i++){
+                    cout << best_ant_sol[i] << " ";
+                }
+                */
+                cout << "Best ant length: " << best_ant_length << endl;
+            }
+        }
+
+        if (best_ant_length < 1.1 * problem->best_known){
+            best_ant_sol = two_five_opt(problem, best_ant_sol);
+        } else {
+            best_ant_sol = two_opt_simplified(problem, best_ant_sol);
+        }
+        int old_ant_length = best_ant_length;
+        best_ant_length = compute_length(problem, best_ant_sol);
+        if (best_ant_length < old_ant_length){
+            cout << "Best ant length: " << best_ant_length << endl;
+        }
+
+        // Update pheromone matrix globally
+        // Evaporate for every edge
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j < n; j++){
+                pheromone_matrix[i][j] *= (1 - phi);
+            }
+        }
+
+        // Update pheromone matrix with best ant's path
+        float constant = phi / best_ant_length;
+        for (int j = 0; j < n - 1; j++){
+            int node_1 = best_ant_sol[j];
+            int node_2 = best_ant_sol[j + 1];
+            pheromone_matrix[node_1][node_2] += constant;
+            pheromone_matrix[node_2][node_1] += constant;
+        }
+        pheromone_matrix[best_ant_sol[n - 1]][best_ant_sol[0]] += constant;
+        pheromone_matrix[best_ant_sol[0]][best_ant_sol[n - 1]] += constant;
+        numIterations++;
+
+        candidate_lists = ESACO_update_candidate_lists(candidate_lists, pheromone_matrix, n, best_ant_sol);
+    }
+    return best_ant_sol;
+}
+
 
 #endif /* ACO_HPP */
--- main.cpp
+++ main.cpp
@@ -205,6 +205,178 @@ vi ACO(Problem * problem, bool local_search)
     return best_ant_sol;
 }
 
+
+
+vi ACO_v2(Problem * problem, bool local_search)
+{
+    float start_time = (float) clock();
+
+    int n = problem->n;
+    int numIterations = 0;       // End condition for the main loop
+    float alpha = 2.0f;
+    float phi = 0.1f;             
+    float beta = 2.0f;              // Importance of distance over pheromone  
+    float rho = 0.1f;               // Evaporation rate
+    int Q0 = 95;                    // Probability of choosing the best solution
+    int m = 10;                     // Number of ants
+
+    // TODO: Initialize pheromone matrix
+    //float tau0 = 1.0f / (n * length_best_NN(problem)); // Initial pheromone value
+    //float tau0 = 1.0f / (n * length_NN(problem, rand() % n)); // Initial pheromone value
+    //float tau0 = 1000 * n / ((float) length_NN(problem, rand() % n)); // Initial pheromone value
+    float tau0 = 1 / (n * (float) length_NN(problem, rand() % n)); // Initial pheromone value
+    //float tau0 = 100.0f;
+    float tau;
+
+    // Pheromone matrix
+    vector<vector<float>> pheromone_matrix(n, vector<float>(n, tau0));
+
+
+    // m = 10 from original paper
+    // Ants and their paths
+    vector<vi> ants(m, vi(n, 0));
+
+    int best_ant_length = INF;      // Global histórico
+    vi best_ant_sol;                // Global histórico    
+
+    int next_city;               
+
+    while (((float) clock() - start_time) / CLOCKS_PER_SEC < 180){         // Run for 3 minutes
+
+        int random_ant = rand() % m;
+
+        for (int k = 0; k < m; k++){        // For each ant
+            uset ant_unvisited;
+            ants[k][0] = rand() % n;
+
+            for (int j = 0; j < n; j++){
+                ant_unvisited.insert(j);
+            }
+            ant_unvisited.erase(ants[k][0]);     // Remove initial city from unvisited
+
+            for (int j = 0; j < n - 1; j++){     // For each city
+                int current_city = ants[k][j];
+
+                // Choose next city
+                // 95% of the time, choose the best edge according to pheromone (exploitation)
+                // 5% of the time, choose a random edge based on probability (exploration)
+                next_city = -1;
+                if (rand() % 100 < Q0){         // Exploitation
+                    // Choose best edge
+                    float best_transition = -1.0f;
+                    uset::iterator it;
+                    for (it = ant_unvisited.begin(); it != ant_unvisited.end(); ++it) {
+                        // edge_transition = tau * (1 / cost)^beta
+                        float edge_transition = pheromone_matrix[current_city][*it] * pow(1.0 / problem->dist_matrix[current_city][*it], beta);
+                        if (edge_transition > best_transition){
+                            best_transition = edge_transition;
+                            next_city = *it;
+                        }
+                    }
+                } else {                     // Exploration
+                    vector<float> probabilities(n, 0);    // Probabiliy of visiting city l from the current city
+                    //int min_dist_to_unvisited = INF;
+                    //int closest_city;
+                    float denominator = 0.0f;
+                    uset::iterator l;
+                    for (l = ant_unvisited.begin(); l != ant_unvisited.end(); ++l) {
+                        /*
+                        int dist = problem->dist_matrix[current_city][*l];
+                        if (dist < min_dist_to_unvisited){
+                            min_dist_to_unvisited = dist;
+                            closest_city = *l;
+                        }
+                        */
+                        denominator += pheromone_matrix[current_city][*l] * pow(1.0 / problem->dist_matrix[current_city][*l], beta);
+                    }
+                    
+                    uset::iterator l2;
+                    for (l2 = ant_unvisited.begin(); l2 != ant_unvisited.end(); ++l2) {
+                        probabilities[*l2] = pheromone_matrix[current_city][*l2] * pow(1.0 / problem->dist_matrix[current_city][*l2], beta) / denominator;
+                    }
+                    //probabilities[closest_city] = 0;    // Don't choose the closest city
+                    
+                    // Create a distribution based on the probabilities
+                    discrete_distribution<int> distribution(probabilities.begin(), probabilities.end());
+                    // Choose a city based on the distribution
+                    next_city = distribution(rng);
+                }
+                
+                // Update ant's path
+                ants[k][j + 1] = next_city;
+                // Remove next city from unvisited
+                ant_unvisited.erase(next_city);
+
+                // Update pheromone matrix
+                tau = (1 - rho) * pheromone_matrix[current_city][next_city] + rho * tau0;
+                pheromone_matrix[current_city][next_city] = tau;
+                pheromone_matrix[next_city][current_city] = tau;
+            }
+
+            // Update pheromone from last city to first city
+            tau = (1 - rho) * pheromone_matrix[ants[k][n - 1]][ants[k][0]] + rho * tau0;
+            pheromone_matrix[ants[k][n - 1]][ants[k][0]] = tau;
+            pheromone_matrix[ants[k][0]][ants[k][n - 1]] = tau;
+
+            if (k == random_ant){
+                //ants[k] = two_five_opt(problem, ants[k]);
+                //ants[k] = two_opt_greedy(problem, ants[k]);
+            }
+
+            // Calculate cost of each ant's path
+            // TODO: improve updating it in the algorithm?
+            int ant_length = compute_length(problem, ants[k]);
+            if (ant_length < best_ant_length){
+                best_ant_length = ant_length;
+                best_ant_sol = ants[k];
+                /*
+                for (int i = 0; i < n; i++){
+                    cout << best_ant_sol[i] << " ";
+                }
+                */
+                cout << "Best ant length: " << best_ant_length << endl;
+            }
+        }
+
+        best_ant_sol = two_five_opt(problem, best_ant_sol);
+        best_ant_sol = two_opt_greedy(problem, best_ant_sol);
+        //cout << "Best ant length: " << best_ant_length << endl;
+
+        // Update pheromone matrix globally
+        // Evaporate for every edge
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j < n; j++){
+                pheromone_matrix[i][j] *= (1 - phi);
+            }
+        }
+
+        // Update pheromone matrix with best ant's path
+        float constant = phi / best_ant_length;
+        for (int j = 0; j < n - 1; j++){
+            int node_1 = best_ant_sol[j];
+            int node_2 = best_ant_sol[j + 1];
+            pheromone_matrix[node_1][node_2] += constant;
+            pheromone_matrix[node_2][node_1] += constant;
+        }
+        pheromone_matrix[best_ant_sol[n - 1]][best_ant_sol[0]] += constant;
+        pheromone_matrix[best_ant_sol[0]][best_ant_sol[n - 1]] += constant;
+        numIterations++;
+
+        //cout << numIterations << "\n";
+    }
+/*
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < n; j++){
+            cout << pheromone_matrix[i][j] << " ";
+        }
+    }
+    cout << "\n";
+
+    cout << "NUMBER OF ITERATIONS: " << numIterations << "\n";
+    */
+    return best_ant_sol;
+}
+
 vi ACO_LS(Problem * problem){
        float start_time = (float) clock();
 
@@ -354,14 +526,15 @@ int main()
     rng.seed(seed);
     cout << "Seed: " << seed << endl;
 
-    write_header("best NN + 2.5-opt + 2-opt", seed);
+    write_header("ESACO", seed);
     for (int i = 0; i < 10; i++) {
         Problem * problem = new Problem(problems[i]);        
         //vi sol = NN(problem, rand()  % problem->n);
         //vi sol = best_NN(problem);
         //sol = two_five_opt(problem, sol);
         //sol = two_opt_greedy(problem, sol);
-        vi sol = ACO_candidate(problem);
+        //vi sol = ACO_candidate(problem);
+        vi sol = ESACO(problem);
         
         if (!verify_sol(problem, sol)) {
             cout << "NOT A VALID SOLUTION FOR " << problem->name << "\n";
